41229308c98e1f47c4910c456743219f0190dd9b
Robert Deans <robdeans@gmail.com>
2017-04-24 18:53:41 -0400

    computer is a worthy adversary

--- TTP TicTacToe/GameViewModel.swift
+++ TTP TicTacToe/GameViewModel.swift
@@ -61,10 +61,11 @@ extension GameViewModel {
                 
                 // Remove from squaresAvailable
                 squaresAvailable.remove(squareButton.square.coordinate)
-                
+
                 // Add squareNum to Set
                 currentPlayer.squares.insert(squareButton.square.number)
-                
+                movesTaken += 1
+
                 /*
                  // Add to player dictionary
                  let column = squareButton.square.coordinate.column
@@ -86,12 +87,21 @@ extension GameViewModel {
                  */
                 
                 // Check for win
+
                 if movesTaken >= 4 {
                     if checkForWin(at: squareButton.square.coordinate) {
                         print("WINNNN")
                         // resetGame()
                         alertViewDelegate?.presentAlert(for: currentPlayer)
+                        return
                     }
+                }
+                
+                // Tie: No winner found after 9 moves
+                print("moves taken = \(movesTaken)")
+                if movesTaken == 9 {
+                    alertViewDelegate?.presentAlert(for: nil)
+                    return
                 }
                 
                 
@@ -104,30 +114,32 @@ extension GameViewModel {
     }
     
     func computerSquareSelection() {
-        let _ = Timer.init(timeInterval: 2, repeats: false) { (timer) in
+       print("computer selecting square")
+        let timer = Timer.init(timeInterval: 2, repeats: false) { (timer) in
             let squaresArray = Array(self.squaresAvailable)
-            let index = arc4random_uniform(UInt32(squaresArray.count))
+            let index = arc4random_uniform(UInt32(squaresArray.count - 1))
+            print("index selected is \(index)")
             let squareCoordinate = squaresArray[Int(index)]
             
+            print("has selected \(squareCoordinate)")
             self.computerTurnDelegate?.computerChoseSquare(at: squareCoordinate)
-            
+            timer.invalidate()
         }
+        timer.fire()
     }
     
     func changeCurrentPlayer() {
-        movesTaken += 1
         if currentPlayer.symbol == store.playerOne.symbol {
             currentPlayer = store.computer
         } else if currentPlayer.symbol == store.computer.symbol {
             currentPlayer = store.playerOne
         }
+        print("current player is now \(currentPlayer.name)")
     }
     
     
     
     func checkForWin(at coordinate: Coordinate) -> Bool {
-        print("checking for win: \(currentPlayer.squares)")
-        
         var didWin = false
         
         // Using Set only
